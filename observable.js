b5a77de73f710d82e5743e9ddb2a49703c1a4d41
FrediL <inferi.gladius@gmail.com>
2013-11-03 22:43:14 +0100

    wrap in function

--- observable.js
+++ observable.js
@@ -1,45 +1,46 @@
 'use strict';
-window['observable']=observable;
-function observable(obj) {
-	var events = {};
+(function(win) {
+	win['observable'] = function(obj) {
+		var events = {};
 
-	obj.on = function(name, callback) {
-		var names = name.split(" ");
-		for (var i in names) {
-			var split = resolveName(names[i])
-			events[split.name] = events[split.name] || [];
-			events[split.name].push({namespace: split.namespace, callback: callback});
+		obj.on = function(name, callback) {
+			var names = name.split(" ");
+			for (var i in names) {
+				var split = resolveName(names[i])
+				events[split.name] = events[split.name] || [];
+				events[split.name].push({namespace: split.namespace, callback: callback});
+			}
+		}
+
+		obj.off = function(name) {
+			if (name.indexOf('.') == -1)
+				delete events[name];
+			else {
+				var split = resolveName(name)
+				for (var i in events[split.name])
+					 if (events[split.name][i].namespace == split.namespace)
+						delete events[split.name][i];
+				if (!events[split.name].length) 
+					delete events[split.name];
+			}
+		}
+
+		obj.trigger = function(name) {
+			var split = resolveName(name);
+			if (events[split.name]) {
+				var args = Array.prototype.slice.call(arguments,1);
+				for (var i in events[split.name])
+					if (split.namespace == '' || events[split.name][i].namespace == split.namespace)
+						events[split.name][i].callback.apply(obj, args);	
+			}
+		}
+
+		function resolveName(name) {
+			var index = name.indexOf('.');
+			if (index == -1)
+				return {name: name, namespace: ''};
+			else
+				return {name: name.substr(0, index), namespace: name.substr(index + 1)};
 		}
 	}
-
-	obj.off = function(name) {
-		if (name.indexOf('.') == -1)
-			delete events[name];
-		else {
-			var split = resolveName(name)
-			for (var i in events[split.name])
-				 if (events[split.name][i].namespace == split.namespace)
-					delete events[split.name][i];
-			if (!events[split.name].length) 
-				delete events[split.name];
-		}
-	}
-
-	obj.trigger = function(name) {
-		var split = resolveName(name);
-		if (events[split.name]) {
-			var args = Array.prototype.slice.call(arguments,1);
-			for (var i in events[split.name])
-				if (split.namespace == '' || events[split.name][i].namespace == split.namespace)
-					events[split.name][i].callback.apply(obj, args);	
-		}
-	}
-
-	function resolveName(name) {
-		var index = name.indexOf('.');
-		if (index == -1)
-			return {name: name, namespace: ''};
-		else
-			return {name: name.substr(0, index), namespace: name.substr(index + 1)};
-	}
-}
+})(window);
